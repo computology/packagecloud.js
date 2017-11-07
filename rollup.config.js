a68c47657ca9aa9d2fdea24656f1d2d5ac940ffe
Armando Canals <armandocanals@gmail.com>
2017-11-06 18:35:15 -0800

    Export browser/node files using a single rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -5,18 +5,27 @@ import replace     from "rollup-plugin-replace"
 import nodeGlobals from "rollup-plugin-node-globals"
 import json        from "rollup-plugin-json"
 
-export default {
+export default [{
   input: 'src/main.js',
   output: {
     file: 'dist/packagecloud.browser.js',
     format: 'iife'
   },
-  plugins: [resolve({browser: true}), replace( {
-              include: '**/node_modules/formidable/lib/**',
-              values: { 'global.GENTLY': 'false' }
-            } ),
-            builtins(),
-            json(),
-            commonjs({include: 'node_modules/**', namedExports: {'superagent': [ 'superagent' ]} }),
-            nodeGlobals()]
-};
+  plugins: [
+    resolve({browser: true}), replace({
+      include: '**/node_modules/formidable/lib/**',
+      values: { 'global.GENTLY': 'false' }
+    }), builtins(),
+    json(),
+    commonjs({include: 'node_modules/**',
+              namedExports: {'superagent': [ 'superagent' ]} }),
+    nodeGlobals()
+  ]
+}, {
+  input: 'src/main.js',
+  output:     {
+    file: 'dist/packagecloud.js',
+    format: 'cjs',
+    external: ['superagent']
+  }
+}];
--- rollup.node.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  input: 'src/main.js',
-  output: {
-    file: 'dist/packagecloud.js',
-    format: 'cjs'
-  },
-  external: ['superagent']
-};
