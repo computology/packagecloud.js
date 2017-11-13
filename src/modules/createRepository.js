68e2c33eec3ea5e38028fcf2fe23272ccd27583f
Armando Canals <armandocanals@gmail.com>
2017-11-12 23:49:26 -0800

    Refactor construction of body for post

--- src/modules/createRepository.js
+++ src/modules/createRepository.js
@@ -11,15 +11,11 @@ export default (request, options) => {
 
   validateOptions(options, ['name']);
 
-  let [privacy, name, body] = [(options.privacy ? true : false),
-                               options.name,
-                               JSON.stringify({
-                                 'repository': {
-                                   'name': name,
-                                   'private': privacy
-                                 }
-                               })];
+  let [privacy, name] = [(options.privacy ? true : false), options.name];
+
+  let payload = {'repository': {'name': name,'private': privacy}};
+  
   return request
     .post(options.baseUrl + "repos.json")
-    .send(body);
+    .send(payload);
 }
