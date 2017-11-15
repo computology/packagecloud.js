1b8596a59274f1d520473f1c8aeffab0332775ee
Armando Canals <armandocanals@gmail.com>
2017-11-14 16:03:56 -0800

    Rename test directories

--- src/modules/test/searchPackages.test.js
+++ src/modules/test/searchPackages.test.js
@@ -10,7 +10,7 @@ describe("Searching for packges in a repository", () => {
     return pc.searchPackages({repo: 'user/repo', filename: 'mypackage'}).then(resolve);
   });
 
-    it('should return a list of all packages in a repo', () => {
+  it('should return a list of all packages in a repo', () => {
     expect.assertions(1);
     let resolve = (data) => {
       expect(data).toBeDefined();
